c4a143513fd566796d2759acb3fcb9cded0892cd
Sigma Park <greengb@naver.com>
2019-10-29 20:56:35 +0900

    Revert "comment1"
    This reverts commit cf2c2052c0f5aec9b885b188052b238d526a7433.

--- HelloAzure/HelloAzure/Ex_Hello.cpp
+++ HelloAzure/HelloAzure/Ex_Hello.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <list>
 
 int main()
 {
@@ -7,16 +6,6 @@ int main()
 
 	std::cout << "I expect you to boost me up.\n";
 
-	std::list<int> li;
-
-	for (int n = 1; li.size() < 10; n++)
-		if (n % 3 == 0)
-			li.emplace_back(n);
-
-	for(auto const& x : li)
-		std::cout << x << ' ';
-
-	std::cout << '\n';
 
 	return 0;
 }
