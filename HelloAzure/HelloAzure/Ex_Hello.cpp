7a5a07b234411f41c6990021d6d5db71ed31733a
Sigma Park <greengb@naver.com>
2019-10-29 20:53:14 +0900

    comment1

--- HelloAzure/HelloAzure/Ex_Hello.cpp
+++ HelloAzure/HelloAzure/Ex_Hello.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 
 int main()
 {
@@ -6,6 +7,16 @@ int main()
 
 	std::cout << "I expect you to boost me up.\n";
 
+	std::list<int> li;
+
+	for (int n = 1; li.size() < 10; n++)
+		if (n % 3 == 0)
+			li.emplace_back(n);
+
+	for(auto const& x : li)
+		std::cout << x << ' ';
+
+	std::cout << '\n';
 
 	return 0;
 }
