f85088a4dca1fde4a98270fd5ba738fd99d76928
Sigma Park <greengb@naver.com>
2019-12-28 22:54:17 +0900

    Mean -   parallelization -   weighting coefficient container -   den_t deduction when weighting mode

--- Armory/Armory.h
+++ Armory/Armory.h
@@ -14,6 +14,7 @@
 #include <numeric>
 #include <cmath>
 #include <limits>
+#include <execution>
 
 
 ////////--////////--////////--////////--////////-#////////--////////--////////--////////--////////-#
@@ -25,14 +26,19 @@ namespace sgm
 	template<unsigned DP, typename T1, typename T2 = T1>
 	static bool is_close(T1 t1, T2 t2)
 	{
+		using std::decay_t;
+
 		static_assert
-		(	(	std::is_convertible_v< std::decay_t<T2>, std::decay_t<T1> >
-			&&	std::is_arithmetic_v< std::decay_t<T1> >
+		(	(	std::is_convertible_v< decay_t<T2>, decay_t<T1> >
+			&&	std::is_arithmetic_v< decay_t<T1> >
 			)
 		,	"types to be compared are not arithmetric or dismatched each other."
 		);
-
-		return abs(t1 - t2) < (T1)pow(10.0, DP) * std::numeric_limits<T1>::epsilon();
+		
+		if constexpr(std::is_floating_point_v< decay_t<T1> >)
+			return abs(t1 - t2) < (T1)pow(10.0, DP) * std::numeric_limits<T1>::epsilon();
+		else if constexpr(std::is_integral_v< decay_t<T1> >)
+			return t1 == t2;
 	}
 
 
@@ -64,19 +70,24 @@ namespace sgm
 	}
 	//--------//--------//--------//--------//-------#//--------//--------//--------//--------//---
 
-	/**
-	*	@brief	average 
-	*	@details	
-	*		It returns mean value of elements in container con as std::optional format.
-	*		You can get a weighted mean by putting its weight function as 2nd parameter.
-	*		It returns std::nullopt when con has no element or the sum of weights are zero.
+
+	/**	Average
+	*	
+	*	It returns mean value of elements in container con as std::optional format.
+	*
+	*	You can get a weighted mean by putting its weight function 
+	*	or a container which has weight values as 2nd parameter.
+	*
+	*	It returns std::nullopt when con has no element or the sum of weights are zero.
+	*
+	*	You can choose execution policy by putting std::execution policy object as 3rd parameter.
 	*		
-	*		Requirements
-	*		1) the container con has iterator method fit to STL
-	*		2) operator+ between elements in con and CON::operator/(den_t) are required.
+	*	Requirements
+	*	1) the container con has iterator method fit to STL
+	*	2) operator+ between elements in con and CON::operator/(den_t) are required.
 	*/
-	template<typename den_t = double, unsigned DP = 1, typename F = nullptr_t, typename CON>
-	static decltype(auto) Mean(CON&& con, [[maybe_unused]] F&& wgt_func = nullptr)
+	template<	typename den_t = double, typename CON, typename W, typename POL>
+	static decltype(auto) Mean(CON&& con, [[maybe_unused]] W&& wgt, POL pol)
 	{
 		using std::cbegin, std::cend, std::decay_t;
 		using elem_t = decay_t<decltype( *cbegin(con) )>;
@@ -84,58 +95,92 @@ namespace sgm
 		if constexpr		// uniformly weighted average
 		(	auto const [eager_reduce, con_size]
 			=	std::make_pair
-				(	[](auto const& c)
+				(	[pol](auto const& c)
 					{
 						assert( cbegin(c) != cend(c) );
 
-						return 
+						return
 						std::reduce
-						(	std::next( cbegin(c) ), cend(c), *cbegin(c)
+						(	pol, std::next( cbegin(c) ), cend(c), *cbegin(c)
 						,	std::plus< decay_t<decltype( *cbegin(c) )> >()
 						);
 					}
 				,	std::distance( cbegin(con), cend(con) )
 				)
-		;	std::is_same_v< decay_t<F>, nullptr_t >
+		;	std::is_same_v< decay_t<W>, nullptr_t >
 		)	
 			return
 			con_size == 0
 			?	std::optional<elem_t>{}
 			:	std::optional<elem_t>( eager_reduce(con) / static_cast<den_t>(con_size) );
-		else if constexpr(std::is_invocable_r_v< den_t, decay_t<F>, elem_t >)
+		//--------//--------//--------//--------//-------#//--------//--------//--------//--------
+		else if constexpr
+		(	auto eager_tran_reduce
+			=	[pol, con_size, &con, &eager_reduce](auto&& wc)
+				{
+					using producted_t 
+					=	decltype
+						(	std::declval< decay_t<decltype( *cbegin(wc) )> >() 
+						*	std::declval<elem_t>()
+						);
+
+					using wgted_t		
+					=	std::conditional_t
+						<	std::is_convertible_v<producted_t, elem_t>, elem_t, producted_t
+						>;	// type casting to prevent side-effect from template expression
+
+					if (con_size == 0)
+						return std::optional<wgted_t>{};
+
+					if( auto const den = eager_reduce(wc); sgm::is_almost_0<1>(den) )
+						return std::optional<wgted_t>{};
+					else
+						return
+						std::optional<wgted_t>
+						(	std::transform_reduce
+							(	pol
+							,	std::next( cbegin(wc) ), cend(wc)
+							,	std::next( cbegin(con) )
+							,	*cbegin(wc) * *cbegin(con) 
+							,	std::plus<wgted_t>()
+							,	[](auto const& w, elem_t const& t)-> wgted_t{  return w*t;  }
+							)
+						/	den
+						);
+				}
+		;	std::is_invocable_v< decay_t<W>, elem_t >
+		)
 		{
-			using producted_t = decltype(std::declval<den_t>() * std::declval<elem_t>());
+			std::vector<  std::invoke_result_t< decay_t<W>, elem_t >  > wcon(con_size);
 
-			using wgted_t		
-			=	std::conditional_t
-				<	std::is_convertible_v<producted_t, elem_t>, elem_t, producted_t
-				>;
+			std::transform( pol, cbegin(con), cend(con), wcon.begin(), std::forward<W>(wgt) );
 
-			auto wgts = reserved_vector<den_t>(con_size);
-
-			if(con_size == 0)
-				return std::optional<wgted_t>{};
-				
-			for(auto const& elem : con)
-				wgts.emplace_back( wgt_func(elem) );
-
-			if( auto const den = eager_reduce(wgts); sgm::is_almost_0<DP>(den) )
-				return std::optional<wgted_t>{};
-			else
-				return
-				std::optional<wgted_t>
-				(	std::inner_product
-					(	std::next( cbegin(wgts) ), cend(wgts), std::next( cbegin(con) )
-					,	*cbegin(wgts) * *cbegin(con) 
-					,	std::plus<wgted_t>()
-					,	[](den_t const& w, elem_t const& elem)-> wgted_t{  return w*elem;  }
-					)
-				/	den
-				);
+			return eager_tran_reduce(wcon);
 		}
-		else static_assert(false, "no suitable method was found.");
+		else
+			return eager_tran_reduce( std::forward<W>(wgt) );
+	}
+
+
+	template<typename den_t = double, typename CON, typename _2ND>
+	static decltype(auto) Mean(CON&& con, _2ND&& second)
+	{
+		using std::forward;
+
+		if constexpr(std::is_execution_policy_v< std::decay_t<_2ND> >)
+			return Mean<den_t>( forward<CON>(con), nullptr_t(), forward<_2ND>(second) );
+		else //	second stands for weighting coefficients.
+			return Mean( forward<CON>(con), forward<_2ND>(second), std::execution::seq );
+	}
+
+
+	template<typename den_t = double, typename CON>
+	static decltype(auto) Mean(CON&& con)
+	{
+		return Mean<den_t>( std::forward<CON>(con), nullptr_t(), std::execution::seq );
 	}
 	//--------//--------//--------//--------//-------#//--------//--------//--------//--------//---
+
 
 }
 
--- Armory/Ex_Armory.cpp
+++ Armory/Ex_Armory.cpp
@@ -9,7 +9,12 @@ int main()
 	<<	is_close<2>(1.0, .99999) << ", " << is_close<5>(1.f, .99999f) << '\n'
 	<<	is_big_enough<1>(std::numeric_limits<int>::max()/1000) << ", "
 	<<	is_big_enough<5>(std::numeric_limits<double>::max()/10.0) << '\n'
-	<<	Mean<float>(std::vector<float>{1,2,3,4,5,6}, [](auto){  return 1.f;  }).value() << '\n'
+	<<	Mean<float>(std::vector<float>{1,2,3,4,5,6}).value() << '\n'
+	<<	Mean(std::vector<float>{1,2,3,4,5,6}, [](auto){  return 1.f;  }).value() << '\n'
+	<<	Mean
+		(	std::vector<float>{1,2,3,4,5,6}
+		,	std::vector<float>{1,1,1,1,1,1}, std::execution::par
+		).value() << '\n'
 	;
 
 	return 0;
