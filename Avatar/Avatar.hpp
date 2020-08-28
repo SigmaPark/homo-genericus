0d5ac744a9287b592ea10e95adc37fff2747621b
Sigma Park <greengb@naver.com>
2020-08-28 21:07:49 +0900

    Signed-off-by: Sigma Park <greengb@naver.com>

--- Avatar/Avatar.hpp
+++ Avatar/Avatar.hpp
@@ -5,197 +5,211 @@
 
 #include <cassert>
 #include "..\Type_Analysis\Type_Analysis.hpp"
-////////--////////--////////--////////--////////-#////////--////////--////////--////////--////////-#
+//========//========//========//========//=======#//========//========//========//========//=======#
+
 
 namespace sgm
 {
-
-
 	SGM_DECL_PROXY_TEMPLATE_CLASS(Avatar);
 
-
 	template<class T>
-	struct is_constAvatar : No_Making
-	{
-	private:
-		template<class T>
-		struct AvtMutability : No_Making
-		{
-			using type 
-			=	std::conditional_t< is_immutable<T>::value && is_Avatar<T>::value, invar, void >;
-		};
-
-		template<class T, class M, bool...BS>
-		struct AvtMutability<  Avatar_t< T, M, Avatar_T_Helper<T, M, BS...> >  > : No_Making
-		{
-			using type 
-			=	std::conditional_t
-				<	std::is_same<M, invar>::value || is_immutable<T>::value
-				,	invar
-				,	std::conditional_t
-					<	is_Avatar<T>::value
-					,	typename AvtMutability< std::remove_reference_t<T> >::type
-					,	Var
-					>
-				>;
-		};
-
-
-	public:
-		enum : bool
-		{	value 
-			=	std::is_same
-				<	typename AvtMutability< std::remove_reference_t<T> >::type, invar					 
-				>::value
-		};
-	};
-	//========//========//========//========//=======#//========//========//========//========//===
-
-
-	template<class T, class M>
-	class Avatar_t< T, M, Avatar_T_Helper<T, M, false, false, false> >
-	{
-	public:
-		enum : bool
-		{	IS_CONST 
-			=	is_constAvatar
-				<	Avatar_t< T, M, Avatar_T_Helper<T, M, false, false, false> >  
-				>::value
-		};
-
-		using value_t = std::conditional_t< IS_CONST, std::add_const_t<T>, T >;
-
-
-		bool is_yet() const	{  return _state == State::YET;  }
-		bool is_owning() const	{  return _state == State::OWNING;  }
-		bool has_gone() const	{  return _state == State::GONE;  }
-
-
-		Avatar_t() : _pval(nullptr), _state(State::YET){}
-
-		template< class Q, class = std::enable_if_t< !is_Avatar<Q>::value >  >
-		Avatar_t(Q& q) : _pval(&q), _state(State::OWNING){}
-
-		Avatar_t(T&&) = delete;
-
-		template<  class M, class = std::enable_if_t< IS_CONST || !Avatar_t<T, M>::IS_CONST >  >
-		Avatar_t(Avatar_t<T, M> const& avt) : _pval(&avt.value()), _state( get_state(avt) ){} 
-
-		~Avatar_t(){  _pval = nullptr, _state = State::GONE;  }
-
-
-		auto value() const-> T const&	{  return *_pval;  }
-		operator T const&() const		{  return value();  }
-
-		template< class = std::enable_if_t<!IS_CONST> >
-		auto value()-> T&{  return *_pval;  }
-
-
-		auto operator=(Avatar_t const& avt)-> Avatar_t&
-		{
-			static_assert(!IS_CONST, "cannot bind to const Avatar_t");
-
-			assert(is_yet() && !avt.has_gone() || is_owning() && avt.is_owning());
-
-			*_pval = avt.value(), _state = avt._state;
-
-			return *this;
-		}
-
-
-		template
-		<	class Q
-		,	class 
-			=	std::enable_if_t
-				<	!IS_CONST && !is_Avatar<Q>::value && std::is_convertible<Q, T>::value
-				>  
-		>
-		auto operator=(Q&& q)-> Avatar_t&
-		{
-			assert(is_owning() && L"Avatar_t has nothing");
-
-			*_pval = std::forward<Q>(q);
-
-			return *this;
-		}
-
-
-		template<class M>
-		auto operator()(Avatar_t<T, M> avt)-> Avatar_t
-		{
-			static_assert
-			(	IS_CONST || !Avatar_t<T, M>::IS_CONST, "cannot bind to const Avatar_t"
-			);
-
-			_pval = &avt.value(), _state = get_state(avt);
-
-			return *this;
-		}
-
-		template< class Q, class = std::enable_if_t< !is_Avatar<Q>::value >  >
-		auto operator()(Q& q)-> Avatar_t
-		{
-			static_assert
-			(	IS_CONST || !is_immutable<decltype(q)>::value, "cannot bind to const Avatar_t"
-			);
-
-			assert(!has_gone() && L"Avatar_t was released already");
-
-			_pval = &q, _state = State::OWNING;
-
-			return *this;
-		}
-
-
-		template<class Q>
-		bool operator==(Q&& q) const{  return *_pval == std::forward<Q>(q);  }
-
-		template<class Q>
-		bool operator!=(Q&& q) const{  return !( *this == std::forward<Q>(q) );  }
-
-
-		template<class M>
-		bool operator==(Avatar_t<T, M> avt) const
-		{  
-			assert(!has_gone() && !avt.has_gone() && L"Avatar_t was released already");
-
-			return *this == avt.value(); 
-		}
-
-		template<class M>
-		bool operator!=(Avatar_t<T, M> avt) const{  return !(*this == avt);  }
-
-
-	private:
-		value_t* _pval;
-		enum class State{YET, OWNING, GONE} _state;
-
-
-		template<class M>
-		static auto get_state(Avatar_t<T, M> avt)-> State
-		{
-			return 
-			avt.is_owning() ? State::OWNING : (avt.is_yet() ? State::YET : State::GONE);
-		}
-	};
-	//========//========//========//========//=======#//========//========//========//========//===
-
+	struct is_constAvatar;
 
 	template<  class T, class = std::enable_if_t< !is_Avatar<T>::value >  >
-	static auto make_Avatar(T& t) SGM_DECLTYPE_AUTO(  Avatar< std::remove_reference_t<T> >(t)  )
+	static auto make_Avatar(T& t)-> Avatar< std::remove_reference_t<T> >;
 
 	template<  class T, class = std::enable_if_t< is_Avatar<T>::value >  >
-	static auto make_Avatar(T t) SGM_DECLTYPE_AUTO(  Avatar<typename T::value_t>(t.value())  )
-	//========//========//========//========//=======#//========//========//========//========//===
+	static auto make_Avatar(T t)-> Avatar<typename T::value_t>;
+}
+//========//========//========//========//=======#//========//========//========//========//=======#
 
 
-	template<class A, class B, class C>
-	struct is_immutable< Avatar_t<A, B, C> > : No_Making
+template<class T>
+struct sgm::is_constAvatar : No_Making
+{
+private:
+	template<class T>
+	struct AvtMutability : No_Making
 	{
-		enum : bool{value = is_constAvatar< Avatar_t<A, B, C> >::value};
+		using type 
+		=	std::conditional_t< is_immutable<T>::value && is_Avatar<T>::value, invar, void >;
+	};
+
+	template<class T, class M, bool...BS>
+	struct AvtMutability<  Avatar_t< T, M, Avatar_T_Helper<T, M, BS...> >  > : No_Making
+	{
+		using type 
+		=	std::conditional_t
+			<	std::is_same<M, invar>::value || is_immutable<T>::value
+			,	invar
+			,	std::conditional_t
+				<	is_Avatar<T>::value
+				,	typename AvtMutability< std::remove_reference_t<T> >::type
+				,	Var
+				>
+			>;
 	};
 
 
-} // end of namespace sgm
+public:
+	enum : bool
+	{	value 
+		=	std::is_same
+			<	typename AvtMutability< std::remove_reference_t<T> >::type, invar					 
+			>::value
+	};
+};
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
+
+
+template<class T, class M>
+class sgm::Avatar_t< T, M, sgm::Avatar_T_Helper<T, M, false, false, false> >
+{
+public:
+	enum : bool
+	{	IS_CONST 
+		=	is_constAvatar
+			<	Avatar_t< T, M, Avatar_T_Helper<T, M, false, false, false> >  
+			>::value
+	};
+
+	using value_t = std::conditional_t< IS_CONST, std::add_const_t<T>, T >;
+
+
+	bool is_yet() const	{  return _state == State::YET;  }
+	bool is_owning() const	{  return _state == State::OWNING;  }
+	bool has_gone() const	{  return _state == State::GONE;  }
+
+
+	Avatar_t() : _pval(nullptr), _state(State::YET){}
+
+	template< class Q, class = std::enable_if_t< !is_Avatar<Q>::value >  >
+	Avatar_t(Q& q) : _pval(&q), _state(State::OWNING){}
+
+	Avatar_t(T&&) = delete;
+
+	template<  class M, class = std::enable_if_t< IS_CONST || !Avatar_t<T, M>::IS_CONST >  >
+	Avatar_t(Avatar_t<T, M> const& avt) : _pval(&avt.value()), _state( get_state(avt) ){} 
+
+	~Avatar_t(){  _pval = nullptr, _state = State::GONE;  }
+
+
+	auto value() const-> T const&	{  return *_pval;  }
+	operator T const&() const		{  return value();  }
+
+	template< class = std::enable_if_t<!IS_CONST> >
+	auto value()-> T&{  return *_pval;  }
+
+
+	auto operator=(Avatar_t const& avt)-> Avatar_t&
+	{
+		static_assert(!IS_CONST, "cannot bind to const Avatar_t");
+
+		assert(is_yet() && !avt.has_gone() || is_owning() && avt.is_owning());
+
+		*_pval = avt.value(), _state = avt._state;
+
+		return *this;
+	}
+
+
+	template
+	<	class Q
+	,	class 
+		=	std::enable_if_t
+			<	!IS_CONST && !is_Avatar<Q>::value && std::is_convertible<Q, T>::value
+			>  
+	>
+	auto operator=(Q&& q)-> Avatar_t&
+	{
+		assert(is_owning() && L"Avatar_t has nothing");
+
+		*_pval = std::forward<Q>(q);
+
+		return *this;
+	}
+
+
+	template<class M>
+	auto operator()(Avatar_t<T, M> avt)-> Avatar_t
+	{
+		static_assert
+		(	IS_CONST || !Avatar_t<T, M>::IS_CONST, "cannot bind to const Avatar_t"
+		);
+
+		_pval = &avt.value(), _state = get_state(avt);
+
+		return *this;
+	}
+
+	template< class Q, class = std::enable_if_t< !is_Avatar<Q>::value >  >
+	auto operator()(Q& q)-> Avatar_t
+	{
+		static_assert
+		(	IS_CONST || !is_immutable<decltype(q)>::value, "cannot bind to const Avatar_t"
+		);
+
+		assert(!has_gone() && L"Avatar_t was released already");
+
+		_pval = &q, _state = State::OWNING;
+
+		return *this;
+	}
+
+
+	template<class Q>
+	bool operator==(Q&& q) const{  return *_pval == std::forward<Q>(q);  }
+
+	template<class Q>
+	bool operator!=(Q&& q) const{  return !( *this == std::forward<Q>(q) );  }
+
+
+	template<class M>
+	bool operator==(Avatar_t<T, M> avt) const
+	{  
+		assert(!has_gone() && !avt.has_gone() && L"Avatar_t was released already");
+
+		return *this == avt.value(); 
+	}
+
+	template<class M>
+	bool operator!=(Avatar_t<T, M> avt) const{  return !(*this == avt);  }
+
+
+private:
+	value_t* _pval;
+	enum class State{YET, OWNING, GONE} _state;
+
+
+	template<class M>
+	static auto get_state(Avatar_t<T, M> avt)-> State
+	{
+		return 
+		avt.is_owning() ? State::OWNING : (avt.is_yet() ? State::YET : State::GONE);
+	}
+};
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
+
+
+template<class T, class>
+auto sgm::make_Avatar(T& t)-> Avatar< std::remove_reference_t<T> >
+{
+	return Avatar< std::remove_reference_t<T> >(t);
+}
+
+template<class T, class>
+auto sgm::make_Avatar(T t)-> Avatar<typename T::value_t>
+{  
+	return Avatar<typename T::value_t>(t.value());
+}
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
+
+
+template<class A, class B, class C>
+struct sgm::is_immutable< sgm::Avatar_t<A, B, C> > : No_Making
+{
+	enum : bool{value = is_constAvatar< Avatar_t<A, B, C> >::value};
+};
+
 
 #endif // end of #ifndef _SGM_AVATAR_
--- Pinweight/Pinweight.hpp
+++ Pinweight/Pinweight.hpp
@@ -5,218 +5,226 @@
 
 #include "..\Type_Analysis\Type_Analysis.hpp"
 #include <atomic>
+//========//========//========//========//=======#//========//========//========//========//=======#
 
-////////--////////--////////--////////--////////-#////////--////////--////////--////////--////////-#
 
 namespace sgm
 {
-
-
 	SGM_DECL_PROXY_TEMPLATE_CLASS(Pinweight);
 
+	template<class T>
+	class _Pinweight_interface;
 
 	template<class T>
-	class _Pinweight_interface
-	{
-	public:
-		using count_t = std::atomic<size_t>;
-		using value_t = T;
-
-
-		_Pinweight_interface() : _cpval(new T const()), _pcount( new count_t(1) ){}
-
-		_Pinweight_interface(T const& t) : _cpval( new T const(t) ), _pcount( new count_t(1) ){}
-
-		_Pinweight_interface(T&& t)
-		:	_cpval(  new T const( std::move(t) )  ), _pcount( new count_t(1) )
-		{}
-
-		_Pinweight_interface(_Pinweight_interface const& pwimpl)
-		:	_cpval(pwimpl._cpval), _pcount( _pcount_up(pwimpl._pcount) )
-		{}
-
-		_Pinweight_interface(_Pinweight_interface&& pwimpl) 
-		:	_cpval(pwimpl._cpval), _pcount(pwimpl._pcount)
-		{
-			pwimpl._cpval = nullptr, pwimpl._pcount = nullptr;
-		}
-
-		~_Pinweight_interface(){  _my_pcount_down();  }
-
-		auto value() const-> T const&{  return *_cpval;  }
-		operator T const&() const{  return value();  }
-
-
-		template<class Q>
-		bool operator==(Q&& q) const{  return *_cpval == std::forward<Q>(q);  }
-
-		template<class Q>
-		bool operator!=(Q&& q) const{  return !( *this == std::forward<Q>(q) );  }
-
-		bool operator==(_Pinweight_interface pw) const{  return *this == *pw._cpval;  }
-		bool operator!=(_Pinweight_interface pw) const{  return !( *this == pw );  }
-
-
-		bool share_with(_Pinweight_interface const& pw) const{  return _cpval == pw._cpval;  }
-		size_t share_count() const{  return *_pcount;  }
-
-
-	protected:
-		T const* _cpval;
-		count_t mutable* _pcount;
-
-
-		static auto _pcount_up(count_t* pn)-> count_t*
-		{
-			if(pn != nullptr)
-				++(*pn);
-
-			return pn;
-		}
-
-		void _my_pcount_down()
-		{
-			if(_pcount == nullptr)
-				return;
-			else if( --(*_pcount) == 0 )
-				delete _cpval, _cpval = nullptr,
-				delete _pcount, _pcount = nullptr;
-		}
-	};
-	//========//========//========//========//=======#//========//========//========//========//===
-
+	struct __Root;
 
 	template<class T>
-	class Pinweight_t< T, invar, Pinweight_T_Helper<T, invar, false, false, false> >
-	:	public _Pinweight_interface<T>
-	{
-		using impl_t = _Pinweight_interface<T>;
-
-		friend class Pinweight_t<T, Var>;
-
-	public:
-		Pinweight_t() = default;
-
-		Pinweight_t(T const& t) : impl_t(t){}
-		Pinweight_t(T&& t) : impl_t( std::move(t) ){}
-
-		Pinweight_t(_Pinweight_interface<T> const& pwimpl) : impl_t(pwimpl){}
-		Pinweight_t(_Pinweight_interface<T>&& pwimpl) : impl_t( std::move(pwimpl) ){}
-
-		Pinweight_t(Pinweight_t const& pw) : impl_t(pw){}
-		Pinweight_t(Pinweight_t&& pw) : impl_t( std::move(pw) ){}
-
-		~Pinweight_t() = default;
-
-
-		void operator=(T) = delete;
-		void operator=(_Pinweight_interface<T>) = delete;
-		auto operator=(Pinweight_t const&)-> Pinweight_t& = delete;
-	};
-	//--------//--------//--------//--------//-------#//--------//--------//--------//--------//---
-
+	using Root_t = typename __Root<T>::type;
 
 	template<class T>
-	class Pinweight_t< T, Var, Pinweight_T_Helper<T, Var, false, false, false> >
-	:	public _Pinweight_interface<T>
+	static auto make_Pinweight(T&& t)-> Pinweight< std::remove_reference_t<T> >;
+}
+//========//========//========//========//=======#//========//========//========//========//=======#
+
+
+template<class T>
+class sgm::_Pinweight_interface
+{
+public:
+	using count_t = std::atomic<size_t>;
+	using value_t = T;
+
+
+	_Pinweight_interface() : _cpval(new T const()), _pcount( new count_t(1) ){}
+
+	_Pinweight_interface(T const& t) : _cpval( new T const(t) ), _pcount( new count_t(1) ){}
+
+	_Pinweight_interface(T&& t)
+	:	_cpval(  new T const( std::move(t) )  ), _pcount( new count_t(1) )
+	{}
+
+	_Pinweight_interface(_Pinweight_interface const& pwimpl)
+	:	_cpval(pwimpl._cpval), _pcount( _pcount_up(pwimpl._pcount) )
+	{}
+
+	_Pinweight_interface(_Pinweight_interface&& pwimpl) 
+	:	_cpval(pwimpl._cpval), _pcount(pwimpl._pcount)
 	{
-		using impl_t = _Pinweight_interface<T>;
+		pwimpl._cpval = nullptr, pwimpl._pcount = nullptr;
+	}
+
+	~_Pinweight_interface(){  _my_pcount_down();  }
+
+	auto value() const-> T const&{  return *_cpval;  }
+	operator T const&() const{  return value();  }
 
 
-		template<class Q>
-		auto _substitute(Q&& q)-> Pinweight_t&
-		{
-			impl_t::_my_pcount_down();
+	template<class Q>
+	bool operator==(Q&& q) const{  return *_cpval == std::forward<Q>(q);  }
 
-			this->_cpval = new T const( std::forward<Q>(q) ), 
-			this->_pcount = new typename impl_t::count_t(1);
+	template<class Q>
+	bool operator!=(Q&& q) const{  return !( *this == std::forward<Q>(q) );  }
 
-			return *this; 			
-		}
+	bool operator==(_Pinweight_interface pw) const{  return *this == *pw._cpval;  }
+	bool operator!=(_Pinweight_interface pw) const{  return !( *this == pw );  }
 
 
-	public:
-		Pinweight_t() = default;
-
-		Pinweight_t(T const& t) : impl_t(t){}
-		Pinweight_t(T&& t) : impl_t( std::move(t) ){}
-
-		Pinweight_t(_Pinweight_interface<T> const& pwimpl) : impl_t(pwimpl){}
-		Pinweight_t(_Pinweight_interface<T>&& pwimpl) : impl_t( std::move(pwimpl) ){}
-
-		Pinweight_t(Pinweight_t const& pw) : impl_t(pw){}
-		Pinweight_t(Pinweight_t&& pw) : impl_t( std::move(pw) ){}
-
-		~Pinweight_t() = default;
+	bool share_with(_Pinweight_interface const& pw) const{  return _cpval == pw._cpval;  }
+	size_t share_count() const{  return *_pcount;  }
 
 
-		auto operator=(T const& t)-> Pinweight_t&{  return _substitute(t);  }
-		auto operator=(T&& t)-> Pinweight_t&{  return _substitute( std::move(t) );  }
-
-	
-		auto operator=(Pinweight_t<T, invar> const& pw)-> Pinweight_t&
-		{
-			if(this->_cpval == pw._cpval)
-				return *this;
-
-			impl_t::_my_pcount_down(), impl_t::_pcount_up(pw._pcount);
-
-			this->_cpval = pw._cpval, this->_pcount = pw._pcount;
-
-			return *this;
-		}
-
-		auto operator=(Pinweight_t<T, invar>&& pw)-> Pinweight_t&
-		{
-			if(this->_cpval == pw._cpval)
-				return *this;
-
-			impl_t::_my_pcount_down();
-
-			this->_cpval = pw._cpval, pw._cpval = nullptr,
-			this->_pcount = pw._pcount, pw._pcount = nullptr;
-
-			return *this;
-		}
-
-		auto operator=(Pinweight_t const& pw)-> Pinweight_t&
-		{
-			return *this = static_cast< Pinweight_t<T, invar> const& >(pw);
-		}
-
-		auto operator=(Pinweight_t&& pw)-> Pinweight_t&
-		{
-			return *this = static_cast< Pinweight_t<T, invar>&& >( std::move(pw) );
-		}
-
-	};
-	//========//========//========//========//=======#//========//========//========//========//===
+protected:
+	T const* _cpval;
+	count_t mutable* _pcount;
 
 
-	template<class T>
-	static auto make_Pinweight(T&& t)-> Pinweight< std::remove_reference_t<T> >
+	static auto _pcount_up(count_t* pn)-> count_t*
 	{
-		return Pinweight< std::remove_reference_t<T> >( std::forward<T>(t) );
+		if(pn != nullptr)
+			++(*pn);
+
+		return pn;
+	}
+
+	void _my_pcount_down()
+	{
+		if(_pcount == nullptr)
+			return;
+		else if( --(*_pcount) == 0 )
+			delete _cpval, _cpval = nullptr,
+			delete _pcount, _pcount = nullptr;
+	}
+};
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
+
+
+template<class T>
+class sgm::Pinweight_t
+<	T, sgm::invar, sgm::Pinweight_T_Helper<T, sgm::invar, false, false, false> 
+> :	public _Pinweight_interface<T>
+{
+	using impl_t = _Pinweight_interface<T>;
+
+	friend class Pinweight_t<T, Var>;
+
+public:
+	Pinweight_t() = default;
+
+	Pinweight_t(T const& t) : impl_t(t){}
+	Pinweight_t(T&& t) : impl_t( std::move(t) ){}
+
+	Pinweight_t(_Pinweight_interface<T> const& pwimpl) : impl_t(pwimpl){}
+	Pinweight_t(_Pinweight_interface<T>&& pwimpl) : impl_t( std::move(pwimpl) ){}
+
+	Pinweight_t(Pinweight_t const& pw) : impl_t(pw){}
+	Pinweight_t(Pinweight_t&& pw) : impl_t( std::move(pw) ){}
+
+	~Pinweight_t() = default;
+
+
+	void operator=(T) = delete;
+	void operator=(_Pinweight_interface<T>) = delete;
+	auto operator=(Pinweight_t const&)-> Pinweight_t& = delete;
+};
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
+
+
+template<class T>
+class sgm::Pinweight_t< T, sgm::Var, sgm::Pinweight_T_Helper<T, sgm::Var, false, false, false> >
+:	public _Pinweight_interface<T>
+{
+	using impl_t = _Pinweight_interface<T>;
+
+
+	template<class Q>
+	auto _substitute(Q&& q)-> Pinweight_t&
+	{
+		impl_t::_my_pcount_down();
+
+		this->_cpval = new T const( std::forward<Q>(q) ), 
+		this->_pcount = new typename impl_t::count_t(1);
+
+		return *this; 			
 	}
 
 
-	template<class T>
-	struct __Root
+public:
+	Pinweight_t() = default;
+
+	Pinweight_t(T const& t) : impl_t(t){}
+	Pinweight_t(T&& t) : impl_t( std::move(t) ){}
+
+	Pinweight_t(_Pinweight_interface<T> const& pwimpl) : impl_t(pwimpl){}
+	Pinweight_t(_Pinweight_interface<T>&& pwimpl) : impl_t( std::move(pwimpl) ){}
+
+	Pinweight_t(Pinweight_t const& pw) : impl_t(pw){}
+	Pinweight_t(Pinweight_t&& pw) : impl_t( std::move(pw) ){}
+
+	~Pinweight_t() = default;
+
+
+	auto operator=(T const& t)-> Pinweight_t&{  return _substitute(t);  }
+	auto operator=(T&& t)-> Pinweight_t&{  return _substitute( std::move(t) );  }
+
+
+	auto operator=(Pinweight_t<T, invar> const& pw)-> Pinweight_t&
 	{
-		using type 
-		=	std::conditional_t
-			<	is_Pinweight<T>::value
-			,	typename __Root<typename T::value_t>::type
-			,	std::decay_t<T>
-			>;
-	};
+		if(this->_cpval == pw._cpval)
+			return *this;
 
-	template<class T> 
-	using Root_t = typename __Root<T>::type;
+		impl_t::_my_pcount_down(), impl_t::_pcount_up(pw._pcount);
+
+		this->_cpval = pw._cpval, this->_pcount = pw._pcount;
+
+		return *this;
+	}
+
+	auto operator=(Pinweight_t<T, invar>&& pw)-> Pinweight_t&
+	{
+		if(this->_cpval == pw._cpval)
+			return *this;
+
+		impl_t::_my_pcount_down();
+
+		this->_cpval = pw._cpval, pw._cpval = nullptr,
+		this->_pcount = pw._pcount, pw._pcount = nullptr;
+
+		return *this;
+	}
+
+	auto operator=(Pinweight_t const& pw)-> Pinweight_t&
+	{
+		return *this = static_cast< Pinweight_t<T, invar> const& >(pw);
+	}
+
+	auto operator=(Pinweight_t&& pw)-> Pinweight_t&
+	{
+		return *this = static_cast< Pinweight_t<T, invar>&& >( std::move(pw) );
+	}
+
+};
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
 
 
+template<class T>
+struct sgm::__Root
+{
+	using type 
+	=	std::conditional_t
+		<	is_Pinweight<T>::value
+		,	typename __Root<typename T::value_t>::type
+		,	std::decay_t<T>
+		>;
+};
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
 
-} // end of namespace sgm
+
+template<class T>
+auto sgm::make_Pinweight(T&& t)-> Pinweight< std::remove_reference_t<T> >
+{
+	return Pinweight< std::remove_reference_t<T> >( std::forward<T>(t) );
+}
+
 
 #endif // end of #ifndef _SGM_PINWEIGHT_
 
