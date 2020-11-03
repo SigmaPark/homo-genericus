6dc360c951055adcf40af1265ca736cab78b7c20
Sigma Park <greengb@naver.com>
2020-11-03 22:35:13 +0900

    Signed-off-by: Sigma Park <greengb@naver.com>

--- indexable/indexable.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef USE_STL_VECTOR_AND_ARRAY
-	//	using standard vector and array
-	#include "indexable_impl_with_STL.hpp"
-#else
-	//	using implementation with C style primitive array
-	#include "indexable_impl.hpp"
-#endif
