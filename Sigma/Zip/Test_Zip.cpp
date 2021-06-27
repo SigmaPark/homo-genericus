8a0085daa8aefa75c66808f35a4e94aef5e49621
Sigma Park <greengb@naver.com>
2021-06-27 19:27:53 +0900

    Zipper

--- Sigma/Zip/Test_Zip.cpp
+++ Sigma/Zip/Test_Zip.cpp
@@ -17,11 +17,17 @@ void Test<1>()
 	std::list<int> Li{2, 4, 6, 8};
 	std::set<int> s{-3, -2, -1, 0};
 
-	//for(  auto&& [a, b, c] : sgm::Zipper( sgm::immut(s), sgm::immut(Li), vec )  )
-	//	c += a+b;
+	for(  auto&& [a, b, c] : sgm::Zipper( sgm::immut(s), sgm::immut(Li), vec )  )
+		c += a+b;
+
+	sgm::spec::is_True( vec.at(0) == 0 && vec.at(1) == 5 && vec.at(2) == 10 && vec.at(3) == 15 );
+}
 
 
-	//sgm::spec::is_True( vec.at(0) == 0 && vec.at(1) == 5 && vec.at(2) == 10 && vec.at(3) == 15 );
+template<>
+void Test<2>()
+{
+	
 }
 
 
@@ -34,6 +40,7 @@ void Test_sgm_Zip::test()
 	try
 	{
 		::Test<1>();
+		::Test<2>();
 
 		std::wcout << L"Zip Test Complete.\n";
 	}
--- Sigma/Zip/Zip.hpp
+++ Sigma/Zip/Zip.hpp
@@ -5,80 +5,201 @@
 
 
 #include "..\Family\Family.hpp"
-#include "..\Functional_iterator\Functional_iterator.hpp"
+#include "..\interface_Traits\interface_Traits.hpp"
+#include <type_traits>
+#include <initializer_list>
 
 
 namespace sgm
 {
 	
+	template<class...CS>  class constZipper;
 	template<class...CS>  class Zipper;
-	//template<class FAM>  class _Zip_Functor;
-	//template<class FAM>
 
+	template<class FAM, bool IS_MUTABLE>  class Zip_iterator;
+
+	struct _itr_fam_traits;
+	
 }
-//========//========//========//========//=======#//========//========//========//========//=======#//========//========
+//========//========//========//========//=======#//========//========//========//========//=======#
 
 
-//template<class FAM>
-//class sgm::_Zip_Functor
-//{
-//public:
-//	_Zip_Functor(FAM *pfam) : _pfam(pfam){}
-//
-//	decltype(auto) operator()(size_t const idx);
-//
-//private:
-//	FAM *_pfam;
-//
-//
-//	template<class...ARGS>
-//	static auto _get([[maybe_unused]] size_t const idx, [[maybe_unused]] FAM &fam, ARGS&...args)
-//	{
-//		size_t constexpr IDX = sizeof...(ARGS);
-//
-//		if constexpr(IDX == std::tuple_size< std::decay_t<FAM> >::value)
-//			return Tie(args...);
-//		else
-//			return _get(  idx, fam, args..., *Next( std::get<IDX>(fam).begin(), idx )  );
-//	
-//	}
-//};
-
-
-template<class...CS>  
-class sgm::Zipper
+template<class FAM, bool IS_MUTABLE>  
+class sgm::Zip_iterator
 {
-	static_assert( (is_iterable<CS>::value &&...) );
+	static_assert(is_Family<FAM>::value);
 
 public:
-	template<class..._CS>
-	Zipper(_CS&&...args) : _fam( std::forward<_CS>(args)... ){}
+	Zip_iterator(FAM fam) : _itr_fam(fam){}
 
+
+	auto operator*() const{  return _deref(*this);  }
+	auto operator*(){  return _deref(*this);  }
+
+	decltype(auto) operator++(){  return _step<true>();  }
+	decltype(auto) operator--(){  return _step<false>();  }
+	auto operator++(int){  auto const clone = *this;  return ++*this,  clone;  }
+	auto operator--(int){  auto const clone = *this;  return --*this,  clone;  }
+
+	auto operator!=(Zip_iterator const zi) const
+	->	bool{  return std::get<0>(this->_itr_fam) != std::get<0>(zi._itr_fam);  }
+
+	auto operator==(Zip_iterator const zi) const-> bool{  return !(*this != zi);  }
 
 
 private:
+	mutable FAM _itr_fam;
+
+
+	template<class ME, class...ARGS>
+	static auto _deref([[maybe_unused]] ME &me, ARGS&...args)
+	{
+		size_t constexpr IDX = sizeof...(ARGS);
+
+		if constexpr(IDX == std::tuple_size<FAM>::value)
+			return Tie(args...);
+		else
+		{
+			using fam_t = Selective_t<IS_MUTABLE, FAM, FAM const>;
+
+			return _deref(  me, args..., *std::get<IDX>( static_cast<fam_t>(me._itr_fam) )  );
+		}
+	}
+
+
+	template<bool PLUS, size_t IDX = 0>
+	auto _step()-> Zip_iterator&
+	{
+		if constexpr(IDX == std::tuple_size<FAM>::value)
+			return *this;
+		else
+		{
+			if constexpr(PLUS)
+				std::get<IDX>(_itr_fam)++; 
+			else
+				std::get<IDX>(_itr_fam)--;
+
+			return _step<PLUS, IDX+1>();
+		}
+	}
+};
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
+
+
+template<class...CS>  
+class sgm::constZipper
+{
+	static_assert( (is_iterable<CS>::value &&...) );
+
+	using _itr_fam_t = Family<  Decay_t< decltype(Declval<CS const>().begin()) >...  >;
+	using _iter_t = Zip_iterator<_itr_fam_t, false>;
+
+public:
+	template<class..._CS>
+	constZipper(_CS&&...args) : _fam( std::forward<_CS>(args)... ), _size(_get_size()){}
+
+	auto cbegin() const-> _iter_t{  return _get_iter<true>(*this);  }
+	auto begin() const{  return cbegin();  }
+
+	auto cend() const-> _iter_t{  return _get_iter<false>(*this);  }
+	auto end() const{  return cend();  }
+
+	auto size() const{  return _size;  }
+
+protected:
 	Family<CS...> _fam;
 
+	template<bool WANT_BEGIN, class ME, class...ARGS>
+	static auto _get_iter([[maybe_unused]] ME &me, ARGS...args)
+	{
+		size_t constexpr IDX = sizeof...(ARGS);
+
+		if constexpr( IDX == sizeof...(CS) )
+			return Make_Family(args...);
+		else if constexpr(WANT_BEGIN)
+			return _get_iter<true>( me, args..., std::get<IDX>(me._fam).begin() );
+		else
+			return _get_iter<false>( me, args..., std::get<IDX>(me._fam).end() );
+	}
 
 
-	//template<size_t IDX = 0>
-	//static auto _read_size(Family<CS...> const &fam)-> size_t
-	//{
-	//	if constexpr( IDX == sizeof...(CS) )
-	//		return std::get<0>(fam).size();
-	//	else if constexpr( is_random_access_iterator< decltype(Declval< Nth_t<IDX, CS...> >().begin()) >::value )
-	//		return std::get<IDX>(fam).size();
-	//	else
-	//		return _read_size<IDX+1>(fam);
-	//}
+private:
+	size_t _size;
+
+	template<size_t IDX = 0>
+	auto _get_size() const
+	{
+		if constexpr( IDX == sizeof...(CS) )
+			return std::get<0>(_fam).size();
+		else if constexpr
+		(	is_random_access_iterator
+			<	decltype(Declval< Nth_t<IDX, CS...> >().begin())  
+			>::	value
+		)
+			return std::get<IDX>(_fam).size();
+		else
+			return _get_size<IDX+1>();
+	}
+};
+
+
+template<class...CS>
+class sgm::Zipper : public constZipper<CS...>
+{
+	using _cz = constZipper<CS...>;
+	using _itr_fam_t = Family<  Decay_t< decltype(Declval<CS>().begin()) >...  >;
+	using _iter_t = Zip_iterator<_itr_fam_t, true>;
+
+public:
+	template<class..._CS>  Zipper(_CS&&...args) : _cz( std::forward<_CS>(args)... ){}
+	
+	auto begin()-> _iter_t{  return _cz::_get_iter<true>(*this);  }
+	auto end()-> _iter_t{  return _cz::_get_iter<false>(*this);  }
 };
 
 
 namespace sgm
 {
+	template<class..._CS>  constZipper(_CS&&...)-> constZipper< remove_aleph_t<_CS&&>... >;
 	template<class..._CS>  Zipper(_CS&&...)-> Zipper< remove_aleph_t<_CS&&>... >;
 }
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
 
 
+#include <iterator>
+
+
+struct sgm::_itr_fam_traits : No_Making
+{
+private:
+	template<class...ITRS>
+	using _tag_t
+	=	Selective_t
+		<	(is_random_access_iterator<ITRS>::value &&...)
+		,	std::random_access_iterator_tag
+		,	Selective_t
+			<	(Has_Operator_Post_Decrease<ITRS>::value &&...)
+			,	std::bidirectional_iterator_tag
+			,	std::forward_iterator_tag
+			>
+		>;	
+
+	template<  class...ITRS, class = Guaranteed_t< (is_iterator<ITRS>::value &&...) >  >
+	/* Declaration Only */ static auto _tag(Family<ITRS...>) noexcept-> _tag_t<ITRS...>;
+
+public:
+	template<class FAM>  using tag_t = decltype( _tag(Declval<FAM>()) );
+};
+
+
+template<bool M, class FAM>
+struct std::iterator_traits< sgm::Zip_iterator<FAM, M> >
+{
+	using iterator_category = typename sgm::_itr_fam_traits::template tag_t<FAM>;
+	using value_type = decay_t<  decltype(*sgm::Declval< sgm::Zip_iterator<FAM, M> >())  >;
+	using difference_type = ptrdiff_t;
+	using pointer = conditional_t<M, value_type, value_type const>*;
+	using reference = remove_pointer_t<pointer>&;	
+};
 
 #endif // end of #ifndef _SGM_ZIP_
