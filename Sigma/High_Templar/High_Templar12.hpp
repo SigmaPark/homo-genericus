5997510bd76b4fc4132a05ed9223d769c1cf927b
Sigma Park <greengb@naver.com>
2021-07-19 00:04:55 +0900

    Filter_iterator

--- Sigma/High_Templar/High_Templar12.hpp
+++ Sigma/High_Templar/High_Templar12.hpp
@@ -9,15 +9,15 @@ namespace sgm
 	namespace ht12
 	{
 		
+		template<class ITR, class FUNC, class DECO>  class Morph_iterator;
 		template<class FS, class RNG, class FUNC, bool ASK_MUTABLE>  class Morph_range;
-		template<class ITR, class PFUNC, class DECO>  class Morph_iterator;
 
-
-		template<class FS, class RNG, class FUNC>
-		class Filter_range;
+		template<class ITR, class FUNC, class DECO>  class Filter_iterator;
+		template<class FS, class RNG, class FUNC>  class Filter_range;
 
 		template<class FS, class...RNGS>
 		class Plait_range;
+
 	}
 }
 //========//========//========//========//=======#//========//========//========//========//=======#
@@ -27,51 +27,67 @@ namespace sgm
 #include <cstdlib>
 
 
-template<class ITR, class PFUNC, class DECO>
+template<class ITR, class FUNC, class DECO>
 class sgm::ht12::Morph_iterator
 {
-	using _Y = decltype( Declval<PFUNC>()(*Declval<ITR>()) );
+	using _Y = decltype( Declval<FUNC>()(*Declval<ITR>()) );
+	using Y = typename DECO::template type<_Y>;
 
-	template<bool IS_MUTABLE>
-	using Y 
-	=	Selective_t
-		<	IS_MUTABLE
-		,	typename DECO::template type<_Y>
-		,	typename DECO::template type<_Y const> 
-		>;
-
-	using iter_t = Morph_iterator;
+	using _Self = Morph_iterator;
 
 public:
-	Morph_iterator(ITR const xitr, PFUNC const *pfunc) : _xitr(xitr), _pfunc(pfunc){}
+	template<  class ITR2, class = Enable_if_t< is_Convertible<ITR2, ITR>::value >  >
+	Morph_iterator(ITR2 const xitr2, FUNC const &func) : _xitr(xitr2), _pfunc(&func){}
 
-	auto operator[](ptrdiff_t const d) const-> Y<false>{  return (*_pfunc)( *(_xitr + d) );  }
-	auto operator[](ptrdiff_t const d)-> Y<true>{  return (*_pfunc)( *(_xitr + d) );  }
-	auto operator*() const-> Y<false>{  return (*this)[0];  }
-	auto operator*()-> Y<true>{  return (*this)[0];  }
 
-	auto operator+(ptrdiff_t const d) const-> iter_t{  return{_xitr + d, _pfunc};  }
-	auto operator-(ptrdiff_t const d) const-> iter_t{  return{_xitr - d, _pfunc};  }
-	auto operator+=(ptrdiff_t const d)-> iter_t&{  return _xitr += d,  *this;  }
-	auto operator-=(ptrdiff_t const d)-> iter_t&{  return _xitr -= d,  *this;  }
+	template
+	<	class ITR2
+	,	class = Enable_if_t< is_Convertible<ITR2, ITR>::value && !is_Same<ITR2, ITR>::value >  
+	>
+	Morph_iterator(Morph_iterator<ITR2, FUNC, DECO> const mitr) 
+	:	_xitr(mitr._xitr), _pfunc(mitr._pfunc){}
 
-	auto operator++()-> iter_t&{  return *this += 1;  }
-	auto operator--()-> iter_t&{  return *this -= 1;  }
-	auto operator++(int)-> iter_t{  iter_t const itr = *this;  return ++*this,  itr;  }
-	auto operator--(int)-> iter_t{  iter_t const itr = *this;  return --*this,  itr;  }
 
-	auto operator-(iter_t const itr) const-> ptrdiff_t{  return _xitr - itr._xitr;  }
+	template
+	<	class ITR2
+	,	class = Enable_if_t< is_Convertible<ITR2, ITR>::value && !is_Same<ITR2, ITR>::value >  
+	>
+	auto operator=(Morph_iterator<ITR2, FUNC, DECO> const mitr)
+	->	_Self&{  return _xitr = mitr._xitr,  _pfunc = mitr._pfunc,  *this;  }
 
-	bool operator==(iter_t const itr) const{  return _xitr == itr._xitr;  }
-	bool operator!=(iter_t const itr) const{  return !(*this == itr);  }
-	bool operator<(iter_t const itr) const{  return _xitr < itr._xitr;  }
-	bool operator>(iter_t const itr) const{  return _xitr > itr._xitr;  }
-	bool operator<=(iter_t const itr) const{  return !(*this > itr);  }
-	bool operator>=(iter_t const itr) const{  return !(*this < itr);  }
 
-private:
+	auto operator*() const-> Y{  return (*_pfunc)(*_xitr);  }
+	auto operator*()-> Y{  return (*_pfunc)(*_xitr);  }
+
+	auto operator++()-> _Self&{  return ++*this;  }
+	auto operator++(int)-> _Self{  _Self const itr = *this;  return ++*this,  itr;  }
+
+	bool operator==(_Self const itr) const{  return _xitr == itr._xitr;  }
+	bool operator!=(_Self const itr) const{  return !(*this == itr);  }
+
+
+	auto operator--()-> _Self&{  return --*this;  }
+	auto operator--(int)-> _Self{  _Self const itr = *this;  return --*this,  itr;  }
+
+
+	auto operator[](ptrdiff_t const d) const-> Y{  return (*_pfunc)( *(_xitr + d) );  }
+	auto operator[](ptrdiff_t const d)-> Y{  return (*_pfunc)( *(_xitr + d) );  }
+
+	auto operator+(ptrdiff_t const d) const-> _Self{  return{_xitr + d, _pfunc};  }
+	auto operator-(ptrdiff_t const d) const-> _Self{  return{_xitr - d, _pfunc};  }
+	auto operator+=(ptrdiff_t const d)-> _Self&{  return _xitr += d,  *this;  }
+	auto operator-=(ptrdiff_t const d)-> _Self&{  return _xitr -= d,  *this;  }
+
+	auto operator-(_Self const itr) const-> ptrdiff_t{  return _xitr - itr._xitr;  }
+
+	bool operator<(_Self const itr) const{  return _xitr < itr._xitr;  }
+	bool operator>(_Self const itr) const{  return _xitr > itr._xitr;  }
+	bool operator<=(_Self const itr) const{  return !(*this > itr);  }
+	bool operator>=(_Self const itr) const{  return !(*this < itr);  }
+
+
 	ITR _xitr;
-	PFUNC const *_pfunc;
+	FUNC const *_pfunc;
 };
 //--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
 
@@ -80,6 +96,45 @@ template<class FS, class RNG, class FUNC, bool ASK_MUTABLE>
 class sgm::ht12::Morph_range
 {
 public:
+	
+};
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
+
+
+template<class ITR, class FUNC, class DECO>
+class sgm::ht12::Filter_iterator
+{
+	using X = typename DECO::template type< decltype(*Declval<ITR>()) >;
+	using _Self = Filter_iterator;
+
+public:
+	template<  class ITR2, class = Enable_if_t< is_Convertible<ITR2, ITR>::value >  >
+	Filter_iterator(ITR2 const xitr2, FUNC const &func) 
+	:	_xitr(xitr2), _pfunc(&func){  _shift_until_valid();  }
+
+
+	template
+	<	class ITR2
+	,	class = Enable_if_t< is_Convertible<ITR2, ITR>::value && !is_Same<ITR2, ITR>::value >  
+	>
+	Filter_iterator(Filter_iterator<ITR2, FUNC, DECO> const fitr) 
+	:	_xitr(fitr._xitr), _pfunc(fitr._pfunc){  _shift_until_valid();  }
+
+
+	template
+	<	class ITR2
+	,	class = Enable_if_t< is_Convertible<ITR2, ITR>::value && !is_Same<ITR2, ITR>::value >  
+	>
+	auto operator=(Filter_iterator<ITR2, FUNC, DECO> const fitr)
+	->	_Self&{  return _xitr = fitr._xitr,  _pfunc = fitr._pfunc,  *this;  }
+
+
+	ITR _xitr;
+	FUNC const *_pfunc;
+
+
+private:
+	void _shift_until_valid(){  for(;  !(*_pfunc)(*_xitr);  _xitr++);  }
 };
 //--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
 
