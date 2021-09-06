477165c6fc75ca31787a3d88443a4119f5e3e804
Sigma Park <greengb@naver.com>
2021-09-06 14:13:33 +0900

    _PinweightBase

--- Sigma/Pinweight/Pinweight.hpp
+++ Sigma/Pinweight/Pinweight.hpp
@@ -14,44 +14,44 @@ namespace sgm
 
 	SGM_ABBREVIABLE_PROXY(Pinweight);
 
-	template<class T>  class _Pinweight_interface;
+	template<class T>  class _PinweightBase;
 
 }
 //========//========//========//========//=======#//========//========//========//========//=======#
 
 
 template<class T>
-class sgm::_Pinweight_interface : public Operator_interface<T const>
+class sgm::_PinweightBase : public Operator_interface<T const>
 {
 public:
 	using count_t = std::atomic<size_t>;
 	using value_t = T;
 
 
-	_Pinweight_interface() 
+	_PinweightBase() 
 	:	_cpval(new value_t()), _pcount( new count_t(1) ){  _update_ptr();  }
 
-	_Pinweight_interface(T const &t) 
+	_PinweightBase(T const &t) 
 	:	_cpval( new value_t(t) ), _pcount( new count_t(1) ){  _update_ptr();  }
 
-	_Pinweight_interface(T &&t) 
+	_PinweightBase(T &&t) 
 	:	_cpval(  new value_t( Move(t) )  ), _pcount( new count_t(1) ){  _update_ptr();  }
 
-	_Pinweight_interface(_Pinweight_interface const &pwimpl)
+	_PinweightBase(_PinweightBase const &pwimpl)
 	:	_cpval(pwimpl._cpval), _pcount( _pcount_up(pwimpl._pcount) ){  _update_ptr();  }
 
-	_Pinweight_interface(_Pinweight_interface &&pwimpl) 
+	_PinweightBase(_PinweightBase &&pwimpl) 
 	:	_cpval(pwimpl._cpval), _pcount(pwimpl._pcount)
 	{
 		_update_ptr(),  pwimpl._cpval = nullptr,  pwimpl._pcount = nullptr;
 	}
 
-	~_Pinweight_interface(){  _my_pcount_down();  }
+	~_PinweightBase(){  _my_pcount_down();  }
 
 
 	auto get() const-> T const&{  return *_cpval;  }
 
-	bool share_with(_Pinweight_interface const &pw) const{  return _cpval == pw._cpval;  }
+	bool share_with(_PinweightBase const &pw) const{  return _cpval == pw._cpval;  }
 	size_t share_count() const{  return *_pcount;  }
 
 
@@ -86,29 +86,29 @@ protected:
 template<class T>
 class sgm::Pinweight_t
 <	T, sgm::invar, sgm::Pinweight_T_Helper<T, sgm::invar, false, false, false> 
-> :	public _Pinweight_interface<T>
+> :	public _PinweightBase<T>
 {
-	using _itfc_t = _Pinweight_interface<T>;
+	using _base_t = _PinweightBase<T>;
 
 	friend class Pinweight_t<T, Var>;
 
 public:
 	Pinweight_t() = default;
 
-	Pinweight_t(T const &t) : _itfc_t(t){}
-	Pinweight_t(T &&t) : _itfc_t( Move(t) ){}
+	Pinweight_t(T const &t) : _base_t(t){}
+	Pinweight_t(T &&t) : _base_t( Move(t) ){}
 
-	Pinweight_t(_Pinweight_interface<T> const &pwitfc) : _itfc_t(pwitfc){}
-	Pinweight_t(_Pinweight_interface<T> &&pwitfc) : _itfc_t( Move(pwitfc) ){}
+	Pinweight_t(_PinweightBase<T> const &pwbase) : _base_t(pwbase){}
+	Pinweight_t(_PinweightBase<T> &&pwbase) : _base_t( Move(pwbase) ){}
 
-	Pinweight_t(Pinweight_t const &pw) : _itfc_t(pw){}
-	Pinweight_t(Pinweight_t &&pw) : _itfc_t( Move(pw) ){}
+	Pinweight_t(Pinweight_t const &pw) : _base_t(pw){}
+	Pinweight_t(Pinweight_t &&pw) : _base_t( Move(pw) ){}
 
 	~Pinweight_t() = default;
 
 
 	void operator=(T) = delete;
-	void operator=(_Pinweight_interface<T>) = delete;
+	void operator=(_PinweightBase<T>) = delete;
 	auto operator=(Pinweight_t const&)-> Pinweight_t& = delete;
 };
 //--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
@@ -116,9 +116,9 @@ public:
 
 template<class T>
 class sgm::Pinweight_t< T, sgm::Var, sgm::Pinweight_T_Helper<T, sgm::Var, false, false, false> >
-:	public _Pinweight_interface<T>
+:	public _PinweightBase<T>
 {
-	using _itfc_t = _Pinweight_interface<T>;
+	using _base_t = _PinweightBase<T>;
 
 
 	template<class Q>
@@ -126,8 +126,8 @@ class sgm::Pinweight_t< T, sgm::Var, sgm::Pinweight_T_Helper<T, sgm::Var, false,
 	{
 		res._my_pcount_down();
 		
-		res._cpval = new typename _itfc_t::value_t( Forward<Q>(q) ),  res._update_ptr();
-		res._pcount = new typename _itfc_t::count_t(1);
+		res._cpval = new typename _base_t::value_t( Forward<Q>(q) ),  res._update_ptr();
+		res._pcount = new typename _base_t::count_t(1);
 
 		return res; 			
 	}
@@ -136,14 +136,14 @@ class sgm::Pinweight_t< T, sgm::Var, sgm::Pinweight_T_Helper<T, sgm::Var, false,
 public:
 	Pinweight_t() = default;
 
-	Pinweight_t(T const &t) : _itfc_t(t){}
-	Pinweight_t(T &&t) : _itfc_t( Move(t) ){}
+	Pinweight_t(T const &t) : _base_t(t){}
+	Pinweight_t(T &&t) : _base_t( Move(t) ){}
 
-	Pinweight_t(_Pinweight_interface<T> const &pwitfc) : _itfc_t(pwitfc){}
-	Pinweight_t(_Pinweight_interface<T> &&pwitfc) : _itfc_t( Move(pwitfc) ){}
+	Pinweight_t(_PinweightBase<T> const &pwbase) : _base_t(pwbase){}
+	Pinweight_t(_PinweightBase<T> &&pwbase) : _base_t( Move(pwbase) ){}
 
-	Pinweight_t(Pinweight_t const &pw) : _itfc_t(pw){}
-	Pinweight_t(Pinweight_t &&pw) : _itfc_t( Move(pw) ){}
+	Pinweight_t(Pinweight_t const &pw) : _base_t(pw){}
+	Pinweight_t(Pinweight_t &&pw) : _base_t( Move(pw) ){}
 
 	~Pinweight_t() = default;
 
@@ -157,9 +157,9 @@ public:
 		if(this->_cpval == pw._cpval)
 			return *this;
 
-		_itfc_t::_my_pcount_down(),  _itfc_t::_pcount_up(pw._pcount);
+		_base_t::_my_pcount_down(),  _base_t::_pcount_up(pw._pcount);
 
-		this->_cpval = pw._cpval,  _itfc_t::_update_ptr(),
+		this->_cpval = pw._cpval,  _base_t::_update_ptr(),
 		this->_pcount = pw._pcount;
 
 		return *this;
@@ -170,9 +170,9 @@ public:
 		if(this->_cpval == pw._cpval)
 			return *this;
 
-		_itfc_t::_my_pcount_down();
+		_base_t::_my_pcount_down();
 
-		this->_cpval = pw._cpval,  _itfc_t::_update_ptr(),  pw._cpval = nullptr,
+		this->_cpval = pw._cpval,  _base_t::_update_ptr(),  pw._cpval = nullptr,
 		this->_pcount = pw._pcount,  pw._pcount = nullptr;
 
 		return *this;
@@ -185,10 +185,10 @@ public:
 	->	Pinweight_t&{  return *this = static_cast< Pinweight_t<T, invar>&& >( Move(pw) );  }
 
 
-	auto mut()-> typename _itfc_t::value_t&
+	auto mut()-> typename _base_t::value_t&
 	{
-		if(_itfc_t::share_count() > 1)
-			*this = Pinweight_t(_itfc_t::get());
+		if(_base_t::share_count() > 1)
+			*this = Pinweight_t(_base_t::get());
 			
 		return *this->_cpval;
 	}
