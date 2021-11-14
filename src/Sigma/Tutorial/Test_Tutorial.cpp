57cc8053100f7192230b9ece1d82bf3ded5f4a38
Sigma Park <greengb@naver.com>
2021-11-14 23:52:28 +0900

    no sgm::fp::compose

---
+++ src/Sigma/Tutorial/Test_Tutorial.cpp
@@ -0,0 +1,17 @@
+#include "Tutorial.hpp"
+#include "..\Specification\Specification.hpp"
+
+
+static void Test01()
+{
+	
+}
+
+
+#include "Test_Tutorial.hpp"
+
+
+SGM_SPEC_TESTS(sgm_Tutorial)
+{	[]{}
+,	::Test01
+};
---
+++ src/Sigma/Tutorial/Test_Tutorial.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+struct Test_sgm_Tutorial
+{
+	Test_sgm_Tutorial() = delete;
+
+	static void test();
+};
