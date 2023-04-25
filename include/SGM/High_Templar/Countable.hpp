e1c0fffccc1d58e681831e4daac471f63ce871a2
Sigma Park <greengb@naver.com>
2023-04-26 00:29:50 +0900

    noexcept on Countable

--- include/SGM/High_Templar/Countable.hpp
+++ include/SGM/High_Templar/Countable.hpp
@@ -39,20 +39,20 @@ private:
 
 
 	template<bool IS_FORWARD, bool PLUS_DIR, class T>
-	static auto shift(T const n, T const diff) noexcept
+	static auto shift(T const n, T const diff) SGM_TRY_NOEXCEPT(n + diff)
 	->	Enable_if_t<IS_FORWARD == PLUS_DIR, T>{  return n + diff;  }
 
 	template<bool IS_FORWARD, bool PLUS_DIR, class T>
-	static auto shift(T const n, T const diff) noexcept
+	static auto shift(T const n, T const diff) SGM_TRY_NOEXCEPT(n - diff)
 	->	Enable_if_t<IS_FORWARD != PLUS_DIR, T>{  return n - diff;  }
 
 
 	template<bool IS_FORWARD, class T>
-	static auto less(T const n1, T const n2) noexcept
+	static auto less(T const n1, T const n2) SGM_TRY_NOEXCEPT(n1 < n2)
 	->	Enable_if_t<IS_FORWARD, bool>{  return n1 < n2;  }
 	
 	template<bool IS_FORWARD, class T>
-	static auto less(T const n1, T const n2) noexcept
+	static auto less(T const n1, T const n2) SGM_TRY_NOEXCEPT(n1 > n2)
 	->	Enable_if_t<!IS_FORWARD, bool>{  return n1 > n2;  }
 };
 //--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
@@ -65,15 +65,32 @@ private:
 	using _itr_t = Count_iterator;
 
 
+	template<bool IS_PLUS_DIR>
+	static auto _shift(T const n, T const diff = 1) SGM_TRY_NOEXCEPT_DECLTYPE_AUTO
+	(	
+		_Count_iterator_Helper::shift<IS_INCREASING, IS_PLUS_DIR>(n, diff)
+	)
+	
+
+	static auto _Less(T const n1, T const n2) SGM_TRY_NOEXCEPT_DECLTYPE_AUTO
+	(
+		_Count_iterator_Helper::less<IS_INCREASING>(n1, n2)
+	)
+
+
+	T _number;
+
+
 public:
 	using value_type = T;
 
 
-	Count_iterator(T const number = std::numeric_limits<T>::max()) : _number(number){}
+	Count_iterator(T const number = std::numeric_limits<T>::max()) SGM_TRY_NOEXCEPT( T(number) )
+	:	_number(number){}
 
-	auto operator*() const noexcept-> T{  return _number;  }
+	auto operator*() const noexcept-> T const&{  return _number;  }
 
-	auto operator++(int) noexcept->	_itr_t
+	auto operator++(int) SGM_TRY_NOEXCEPT( _shift<true>(*Declval<_itr_t>()) )-> _itr_t
 	{  
 		auto const iter = *this;
 
@@ -81,7 +98,7 @@ public:
 	}
 
 
-	auto operator--(int) noexcept-> _itr_t
+	auto operator--(int) SGM_TRY_NOEXCEPT( _shift<false>(*Declval<_itr_t>()) )-> _itr_t
 	{
 		auto const iter = *this;
 
@@ -90,15 +107,54 @@ public:
 
 
 	template<class _T>
-	auto operator+(_T const diff) const noexcept
+	auto operator+(_T const diff) const 
+	SGM_TRY_NOEXCEPT( _shift<true>(*Declval<_itr_t>(), Declval<T>()) )
 	->	_itr_t{  return {_shift<true>( **this, static_cast<T>(diff) )};  }
 
 	template<class _T>
-	auto operator-(_T const diff) const noexcept
+	auto operator-(_T const diff) const 
+	SGM_TRY_NOEXCEPT( _shift<false>(*Declval<_itr_t>(), Declval<T>()) )
 	->	_itr_t{  return {_shift<false>( **this, static_cast<T>(diff) )};  }
 
 
-	auto operator-(_itr_t const itr) const noexcept-> ptrdiff_t
+	template<class _T>
+	auto operator+=(_T const diff) SGM_TRY_NOEXCEPT(Declval<_itr_t>() + diff)
+	->	_itr_t&{  return *this = *this + diff;  }
+	
+	template<class _T>
+	auto operator-=(_T const diff) SGM_TRY_NOEXCEPT(Declval<_itr_t>() - diff)
+	->	_itr_t&{  return *this = *this - diff;  }
+
+	auto operator++() SGM_TRY_NOEXCEPT(Declval<_itr_t>() += 1)-> _itr_t&{  return *this += 1;  }
+	auto operator--() SGM_TRY_NOEXCEPT(Declval<_itr_t>() -= 1)-> _itr_t&{  return *this -= 1;  }
+
+	auto operator[](ptrdiff_t const diff) const SGM_TRY_NOEXCEPT( *(Declval<_itr_t>() + diff) )
+	->	T const&{  return *( *this + diff );  }
+
+
+	auto operator==(_itr_t const itr) const SGM_TRY_NOEXCEPT(*itr == *itr)
+	->	bool{  return **this == *itr;  }
+	
+	auto operator!=(_itr_t const itr) const SGM_TRY_NOEXCEPT( !(itr == itr) )
+	->	bool{  return !(*this == itr);  }
+	
+
+	auto operator<(_itr_t const itr) const SGM_TRY_NOEXCEPT( _Less(itr, itr) )
+	->	bool{  return _Less(*this, itr);  }
+	
+	auto operator>(_itr_t const itr) const SGM_TRY_NOEXCEPT( _Less(itr, itr) )
+	->	bool{  return _Less(itr, *this);  }
+	
+	auto operator<=(_itr_t const itr) const SGM_TRY_NOEXCEPT( !(itr > itr) )
+	->	bool{  return !(*this > itr);  }
+	
+	auto operator>=(_itr_t const itr) const SGM_TRY_NOEXCEPT( !(itr < itr) )
+	->	bool{  return !(*this < itr);  }
+
+
+	auto operator-(_itr_t const itr) const 
+	noexcept( noexcept(*itr > *itr) && noexcept(*itr - *itr) )
+	->	ptrdiff_t
 	{
 		bool const greater_mine = **this > *itr;
 		size_t const diff_ULL = greater_mine ? **this - *itr : *itr - **this;
@@ -112,39 +168,6 @@ public:
 
 		return IS_INCREASING == greater_mine  ?  diff  :  -diff;
 	}
-
-
-	template<class _T>
-	auto operator+=(_T const diff) noexcept-> _itr_t&{  return *this = *this + diff;  }
-	
-	template<class _T>
-	auto operator-=(_T const diff) noexcept-> _itr_t&{  return *this = *this - diff;  }
-
-	auto operator++() noexcept-> _itr_t&{  return *this += 1;  }
-	auto operator--() noexcept-> _itr_t&{  return *this -= 1;  }
-
-	auto operator[](ptrdiff_t const diff) const noexcept-> T{  return *( *this + diff );  }
-
-	auto operator==(_itr_t const itr) const noexcept-> bool{  return **this == *itr;  }
-	auto operator!=(_itr_t const itr) const noexcept-> bool{  return !(*this == itr);  }
-	
-	auto operator<(_itr_t const itr) const noexcept-> bool{  return _Less(*this, itr);  }
-	auto operator>(_itr_t const itr) const noexcept-> bool{  return _Less(itr, *this);  }
-	auto operator<=(_itr_t const itr) const noexcept-> bool{  return !(*this > itr);  }
-	auto operator>=(_itr_t const itr) const noexcept-> bool{  return !(*this < itr);  }
-
-
-private:
-	T _number;
-
-
-	template<bool IS_PLUS_DIR>
-	static auto _shift(T const n, T const diff = 1) noexcept
-	->	T{  return _Count_iterator_Helper::shift<IS_INCREASING, IS_PLUS_DIR>(n, diff);  }
-
-
-	static auto _Less(T const n1, T const n2) noexcept
-	->	bool{  return _Count_iterator_Helper::less<IS_INCREASING>(n1, n2);  }
 };
 //--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
 
@@ -153,24 +176,38 @@ template<class T>
 class sgm::Countable
 {
 public:
-	constexpr Countable(T const length, T const offset = 0) : _length(length), _offset(offset){}
+	constexpr Countable(T const length, T const offset = 0) SGM_TRY_NOEXCEPT( T(length) )
+	:	_length(length), _offset(offset){}
+
 
 	using iterator = Count_iterator<T, true>;
 	using reverse_iterator = Count_iterator<T, false>;
 	using const_iterator = iterator;
 	using const_reverse_iterator = reverse_iterator;
 
-	auto cbegin() const noexcept-> iterator{  return {_offset};  }
-	auto begin() const noexcept-> iterator{  return cbegin();  }
-	auto cend() const noexcept-> iterator{  return {_offset + _length};  }
-	auto end() const noexcept-> iterator{  return cend();  }
 
-	auto crbegin() const noexcept-> reverse_iterator{  return {_offset + _length - 1};  }
-	auto rbegin() const noexcept-> reverse_iterator{  return crbegin();  }
-	auto crend() const noexcept-> reverse_iterator{  return {_offset - 1};  }
-	auto rend() const noexcept-> reverse_iterator{  return crend();  }
+	auto cbegin() const SGM_TRY_NOEXCEPT( iterator{T(0)} )
+	->	iterator{  return {_offset};  }
+	
+	auto begin() const SGM_TRY_NOEXCEPT_DECLTYPE_AUTO(  cbegin()  )
+	
+	auto cend() const SGM_TRY_NOEXCEPT( iterator{T(0)} )
+	->	iterator{  return {_offset + _length};  }
+	
+	auto end() const SGM_TRY_NOEXCEPT_DECLTYPE_AUTO(  cend()  )
+
+	auto crbegin() const SGM_TRY_NOEXCEPT( reverse_iterator{T(0)} )
+	->	reverse_iterator{  return {_offset + _length - 1};  }
+
+	auto rbegin() const SGM_TRY_NOEXCEPT_DECLTYPE_AUTO(  crbegin()  )
+
+	auto crend() const SGM_TRY_NOEXCEPT( reverse_iterator{T(0)} )
+	->	reverse_iterator{  return {_offset - 1};  }
+
+	auto rend() const SGM_TRY_NOEXCEPT_DECLTYPE_AUTO(  crend()  )
 
 	auto size() const noexcept-> size_t{  return _length;  }
+
 
 private:
 	T _length, _offset;
