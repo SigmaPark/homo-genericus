3f4b670051a45ac7a56d38caf54f4b55f0c25d95
Sigma Park <sigma@gm.gist.ac.kr>
2025-10-22 23:37:34 +0900

    add code and files for CV_Complete_Ptr

---
+++ include/SGM/CV_Complete_Ptr/CV_Complete_Ptr.hpp
@@ -0,0 +1,20 @@
+/*  SPDX-FileCopyrightText: (c) 2025 Jin-Eon Park <greengb@naver.com> <sigma@gm.gist.ac.kr>
+*   SPDX-License-Identifier: MIT License
+*/
+//========//========//========//========//=======#//========//========//========//========//=======#
+
+
+#pragma once
+#ifndef _SGM_CV_COMPLETE_PTR_
+#define _SGM_CV_COMPLETE_PTR_
+
+
+namespace sgm
+{
+
+	//	CV_Complete_Ptr implementation to be added here
+
+}
+
+
+#endif
---
+++ test/SGM/CV_Complete_Ptr/Test_CV_Complete_Ptr.hpp
@@ -0,0 +1,19 @@
+/*  SPDX-FileCopyrightText: (c) 2025 Jin-Eon Park <greengb@naver.com> <sigma@gm.gist.ac.kr>
+*   SPDX-License-Identifier: MIT License
+*/
+//========//========//========//========//=======#//========//========//========//========//=======#
+
+
+#pragma once
+#include "SGM/How2use.hpp"
+
+
+namespace sgm
+{
+	namespace h2u
+	{
+
+		SGM_HOW2USE_CLASS(Test_, CV_Complete_Ptr, /**/);
+
+	}
+}
