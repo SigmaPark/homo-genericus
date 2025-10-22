436434519e95c6456892f17cae3560e439f1214d
Sigma Park <sigma@gm.gist.ac.kr>
2025-10-22 23:17:15 +0900

    interface: add a test file

---
+++ test/SGM/CV_Complete_Ptr/Test_CV_Complete_Ptr.cpp
@@ -0,0 +1,27 @@
+/*  SPDX-FileCopyrightText: (c) 2025 Jin-Eon Park <greengb@naver.com> <sigma@gm.gist.ac.kr>
+*   SPDX-License-Identifier: MIT License
+*/
+//========//========//========//========//=======#//========//========//========//========//=======#
+
+
+#include "SGM/CV_Complete_Ptr/CV_Complete_Ptr.hpp"
+#include "Test_CV_Complete_Ptr.hpp"
+
+
+static void intro()
+{
+
+}
+
+
+static void CV_completeness()
+{
+	
+}
+//========//========//========//========//=======#//========//========//========//========//=======#
+
+
+SGM_HOW2USE_TESTS(sgm::h2u::Test_, CV_Complete_Ptr, /**/)
+{	::intro
+,	::CV_completeness
+};
