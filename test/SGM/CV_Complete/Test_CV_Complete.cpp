eb09be8ab2d7eec6f087947eeedd8c642163cdf4
Sigma Park <sigma@gm.gist.ac.kr>
2025-10-23 23:50:34 +0900

    test: Add Pimpl false const problem demonstration to CV_Complete tests

--- test/SGM/CV_Complete/Test_CV_Complete.cpp
+++ test/SGM/CV_Complete/Test_CV_Complete.cpp
@@ -31,13 +31,14 @@ static void intro()
 	<<	L"across different pointer-like types in generic code."
 	<<	sgm::h2u::empty_line;
 }
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
 
 
-static void raw_pointer_tests()
+static void Raw_pointer_tests()
 {
 	sgm::h2u::mdo << sgm::h2u::Title(L"Raw Pointer Tests", 2);
 
-BEGIN_CODE_BLOCK(raw_pointer_tests)
+BEGIN_CODE_BLOCK(Raw_pointer_tests)
 	int value = 42;
 	int* ptr = &value;
 
@@ -76,8 +77,9 @@ BEGIN_CODE_BLOCK(raw_pointer_tests)
 		);
 		SGM_H2U_ASSERT(*cv_wrapper == 42);
 	}
-END_CODE_BLOCK_AND_LOAD(raw_pointer_tests)
+END_CODE_BLOCK_AND_LOAD(Raw_pointer_tests)
 }
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
 
 
 static void unique_ptr_tests()
@@ -115,6 +117,7 @@ END_CODE_BLOCK_AND_LOAD(unique_ptr_tests)
 	<<	L"This is a design limitation - volatile wrappers only work with raw pointers."
 	<<	sgm::h2u::empty_line;
 }
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
 
 
 static void vector_iterator_tests()
@@ -151,12 +154,160 @@ END_CODE_BLOCK_AND_LOAD(vector_iterator_tests)
 	<<	L"This is a design limitation - volatile wrappers only work with raw pointers."
 	<<	sgm::h2u::empty_line;
 }
+//--------//--------//--------//--------//-------#//--------//--------//--------//--------//-------#
+
+
+namespace _detail_false_constness
+{
+
+BEGIN_CODE_BLOCK(pimpl_with_false_constness)
+	// Traditional Pimpl implementation
+	class Traditional_Pimpl_Foo
+	{
+		class impl;
+		impl* _pimpl;
+
+	public:
+		Traditional_Pimpl_Foo();
+		~Traditional_Pimpl_Foo();
+
+		// This allows mutation even when called on const object!
+		// (False const problem)
+		auto value() const-> int&;
+	};
+
+
+	class Traditional_Pimpl_Foo::impl
+	{
+		int _value;
+
+	public:
+		impl() : _value{42}{}
+		auto value()-> int&{  return _value;  }
+	};
+
+
+	Traditional_Pimpl_Foo::Traditional_Pimpl_Foo() : _pimpl{new impl{}}
+	{}
+
+	Traditional_Pimpl_Foo::~Traditional_Pimpl_Foo()
+	{
+		delete _pimpl;
+	}
+
+	auto Traditional_Pimpl_Foo::value() const-> int&{  return _pimpl->value();  }
+END_CODE_BLOCK(pimpl_with_false_constness)	
+
+BEGIN_CODE_BLOCK(pimpl_with_cv_complete)
+	// CV_Complete-based Pimpl implementation
+	class CV_Complete_Pimpl_Foo
+	{
+		class impl;
+		sgm::CV_Complete<impl*> _pimpl;
+
+	public:
+		CV_Complete_Pimpl_Foo();
+		~CV_Complete_Pimpl_Foo();
+
+		// This properly returns int const& when called on const object
+		auto value()-> int&;
+		auto value() const-> int const&;
+	};
+
+
+	class CV_Complete_Pimpl_Foo::impl
+	{
+		int _value;
+
+	public:
+		impl() : _value{100}{}
+		auto value()-> int&{  return _value;  }
+		auto value() const-> int const&{  return _value;  }
+	};
+
+
+	CV_Complete_Pimpl_Foo::CV_Complete_Pimpl_Foo() : _pimpl{new impl{}}
+	{}
+
+	CV_Complete_Pimpl_Foo::~CV_Complete_Pimpl_Foo()
+	{
+		delete _pimpl.base;
+	}
+
+	auto CV_Complete_Pimpl_Foo::value()-> int&{  return _pimpl->value();  }
+	auto CV_Complete_Pimpl_Foo::value() const-> int const&{  return _pimpl->value();  }	
+END_CODE_BLOCK(pimpl_with_cv_complete)
+
+}
+
+
+static void Pimpl_false_const_comparison()
+{
+	sgm::h2u::mdo << sgm::h2u::Title(L"Pimpl False Const Problem Comparison", 2);
+
+	sgm::h2u::mdo
+	<<	L"The traditional Pimpl idiom using raw pointers has a **false const problem**. "
+	<<	L"When the outer class is const-qualified, the pointer itself becomes const, "
+	<<	L"but the pointed-to object remains mutable. This breaks const-correctness." 
+	<<	sgm::h2u::newl
+	<<	sgm::h2u::Load_code_block(L"pimpl_with_false_constness")
+	<<	sgm::h2u::empty_line;
+
+	sgm::h2u::mdo
+	<<	L"CV_Complete solves this by properly propagating CV-qualifiers through "
+	<<	L"the indirection, maintaining true const-correctness." << sgm::h2u::newl
+	<<	sgm::h2u::Load_code_block(L"pimpl_with_cv_complete")
+	<<	sgm::h2u::empty_line;
+
+	using namespace _detail_false_constness;
+
+BEGIN_CODE_BLOCK(comparison_pimpl_tests)
+	// ========== Comparison Test ==========
+
+	// Traditional Pimpl: False const problem
+	{
+		Traditional_Pimpl_Foo const const_obj;
+
+		// Despite const_obj being const, we can still modify the value!
+		// This is the false const problem.
+		const_obj.value() = 999;  // Compiles! (BAD)
+
+		SGM_H2U_ASSERT(const_obj.value() == 999);
+	}
+
+	// CV_Complete Pimpl: True const correctness
+	{
+		CV_Complete_Pimpl_Foo const const_obj;
+
+		static_assert
+		(	sgm::is_Same<decltype(const_obj.value()), int const&>::value
+		, 	"Constness from CV_Complete_Pimpl_Foo::impl sould be preserved." 
+		);
+
+		// const_obj.value() = 999;  // Would NOT compile! (GOOD)
+
+		// Non-const object allows modification
+		CV_Complete_Pimpl_Foo mutable_obj;
+
+		mutable_obj.value() = 999;
+		SGM_H2U_ASSERT(mutable_obj.value() == 999);
+	}
+END_CODE_BLOCK_AND_LOAD(comparison_pimpl_tests)
+
+	sgm::h2u::mdo
+	<<	L"**Key takeaway:** CV_Complete enables proper const-correctness in Pimpl, "
+	<<	L"preventing accidental mutations through const interfaces. "
+	<<	L"Traditional raw pointer Pimpl allows mutation of the pointed-to object "
+	<<	L"even when accessed through a const method."
+	<<	sgm::h2u::empty_line;
+}
 //========//========//========//========//=======#//========//========//========//========//=======#
 
 
 SGM_HOW2USE_TESTS(sgm::h2u::Test_, CV_Complete, /**/)
 {	::intro
-,	::raw_pointer_tests
+,	::Raw_pointer_tests
 ,	::unique_ptr_tests
 ,	::vector_iterator_tests
+,	::Pimpl_false_const_comparison
 };
